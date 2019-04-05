8299f7eb682406c8e0c28fbd5c14a812de8f3726
Victor <v6_dev@inbox.ru>
2019-04-06 00:30:40 +0300

    check

---
+++ features/client_jobs_feature.feature
@@ -0,0 +1,28 @@
+#Feature: Client Jobs
+#
+#  Scenario: Add new job advertisement
+#     Given an order parameters
+#        | name        |
+#        | description |
+#        | price       |
+#        | pay type    |
+#        | type        |
+#        | draft       |
+#    When we create a new job advertisement with name we need indian developer pls
+#    Then service will add it to list of job offers
+
+Feature: Client Jobs
+    Scenario: Post a new job to the website
+      Given: I have a set of jobs
+          | job_title      | job_description            | job_price | job_pay_type | job_category     | req_skills | job_deadline |
+          | JS Developer   | We need to ...             | 350       | fixed        | web_development  | javascript | 3 days       |
+          | UI Designer    | Our website main page ...  | 15        | hourly       | graphic_design   | sketch     | 5 hours      |
+          | Web Copywriter | We start a business on ... | 10        | hourly       | copywriting      | wordpress  | 24 hours     |
+      When: I create job advertisement
+      Then: The set of active jobs for my account includes
+          | JS Developer   |
+          | UI Designer    |
+          | Web Copywriter |
+
+
+
--- features/steps/client_jobs.py
+++ features/steps/client_jobs.py
@@ -2,16 +2,48 @@
 from behave import when, then, given
 
 
-@given('an order parameters')
-def param_get(context):
+# @given('an order parameters')
+# def param_get(context):
+#     pass
+#
+# @when('we create a new job advertisement with name {thename}')
+# def create_job(context, thename):
+#     context.page = context.client.get('/client_jobs/post', follow_redirects=True) #сделаете апишку плез
+#     assert context.page
+#
+#
+# @then('service will add it to list of job offers')
+# def post_job(context):
+#     pass
+
+
+# делаем на паре:
+
+# у нас нет таблицы со списком работ клиента
+
+@given('I have a set of jobs')
+def step_impl(context):
+    for row in context.table:
+        try:
+            Job.get(Job.name == row['job_title'])
+        except Job.DoesNotExist:
+            Job.create(job_title=row['job_title'],
+                           job_description=row['job_description'],
+                           job_price=row['job_price'],
+                           job_pay_type=row['job_pay_type'],
+                           job_category=row['job_category'],
+                           req_skills=row['req_skills'],
+                       job_deadline=row['job_deadline'])
+
+
+@when('I create job advertisement')
+def step_imp(context):
     pass
 
-@when('we create a new job advertisement with name {thename}')
-def create_job(context, thename):
-    context.page = context.client.get('/client_jobs/post', follow_redirects=True) #сделаете апишку плез
-    assert context.page
-
-
-@then('service will add it to list of job offers')
-def post_job(context):
-    pass
+@then('The set of active jobs for my account includes')
+def step_impl(context, name):
+    try:
+        context.result = Job.select().where(Job.name == name)
+    except Job.DoesNotExist:
+        context.result = None
+    assert context.result is not None
